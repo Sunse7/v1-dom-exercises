0d8c17c69f6f1af09070f1c0395e6b179cfef166
Sunse7 <sunsetluna12@gmail.com>
2023-01-04 19:29:06 +0100

    Update script.js

--- sinus-minishop/js/script.js
+++ sinus-minishop/js/script.js
@@ -1,1 +1,10 @@
 // Code here!
+
+let namePotato = document.querySelectorAll('article h3')[0];
+namePotato.innerHTML = 'Potato';
+
+let buttonChange = document.getElementsByTagName('button')[1];
+buttonChange.innerHTML = 'Yay!';
+buttonChange.style.backgroundColor = 'hotpink';
+
+let allPColor = document.querySelectorAll('p');
